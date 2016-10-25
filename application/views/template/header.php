392f3d9fe0f6b6c28d291697a2401301c94441bc
Huge <1004050187@qq.com>
2016-10-25 20:10:09 +0800

    link css

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>创建活动</title>
-<<<<<<< HEAD
-    <link rel="stylesheet" href="<?php echo FCPATH;?>css/bootstrap.css">
-=======
+    <title><?php echo $title?></title>
     <link rel="stylesheet" href="<?php echo base_url("css/bootstrap.css"); ?>">
->>>>>>> origin/master
-
+    <link rel="stylesheet" href="<?php echo base_url("css/create.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("css/font-awesome.min.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("css/detail.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("css/login&register.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("css/personal.css"); ?>">
 </head>
 <body>
