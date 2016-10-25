55149bec6bfa4526f4c93a2854de4ee2bdafb551
AndersonZhangyq <zhangyiqing818@sina.com>
2016-10-25 23:37:40 +0800

    Update one php

--- application/views/person_related/personal_info.php
+++ application/views/person_related/personal_info.php
@@ -1,53 +1,53 @@
-<table border=2 align=center width=60% height="600">
-    <tr>
-        <td>
-            <li><a href="<?php echo site_url('user/info'); ?>">个人信息</a></li>
-        </td>
-        <td rowspan=5 align="center">
-            <!-- 使用php代码完成这部分数据的填写 -->
-            <img align="center" width=100 height=100 src="image/01.png" alt="头像">
-            <table cellspacing="10px">
-                <tr>
-                    <th>电子邮箱</th>
-                    <td><?php echo $user_info['email']; ?></a></td>
-                </tr>
-                <tr>
-                    <th>昵称</th>
-                    <td><?php echo $user_info['nick_name']; ?></a></td>
-                </tr>
-                <tr>
-                    <th>兴趣</th>
-                    <td><?php foreach ($user_info['interest'] as $single_interest):
-                            echo $single_interest['name'] . "  ";
-                        endforeach; ?></td>
-                </tr>
-                <tr>
-                    <th>手机号</th>
-                    <td><?php echo $user_info['phone_number']; ?></td>
-                </tr>
-            </table>
-            <input type="button" value="Submit">
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <li><a href="<?php echo site_url('user/applied'); ?>">我参加的活动</a></li>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <li><a href="<?php echo site_url('user/joined'); ?>">我报名的活动</a></li>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <li><a href="<?php echo site_url('user/favorites'); ?>">收藏活动</a></li>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <li><a href="<?php echo site_url('user/comments'); ?>">评论活动</a></li>
-        </td>
-    </tr>
+<div style="height: 72px;"></div>
+<!--个人中心导航栏-->
+<div class="personal_nav">
+    <div class="personal_title">
+        <li><a class="thisover" href="<?php echo site_url('user/info'); ?>"><span>个人信息</span></a></li>
+    </div>
+    <div class="personal_title">
+        <li><a class="thisover" href="<?php echo site_url('user/applied'); ?>"><span>我参加的活动</span></a></li>
+    </div>
+    <div class="personal_title">
+        <li><a class="thisover" href="<?php echo site_url('user/joined'); ?>"><span>我报名的活动</span></a></li>
+    </div>
+    <div class="personal_title">
+        <li><a class="thisover" href="<?php echo site_url('user/favorites'); ?>"><span>收藏活动</span></a></li>
+    </div>
+    <div class="personal_title">
+        <li><a class="thisover" href="<?php echo site_url('user/comments'); ?>"><span>评论活动</span></a></li>
+    </div>
+</div>
+<!--个人中心导航栏-->
+<!--个人中心主页-->
+<div class="personal_main">
+    <div class="personal-data">
+        <div class="head_portrait" align="center">
+            <img alt="用户昵称" src="../img/IMG_1035.JPG" width="150px" height="150px" style="border-radius:50%;">
+        </div>
+        <div class="p_data">
+            <ul>
+                <li>邮箱:<div class="p_present"><?php echo $user_info['email']; ?></div></li>
+                <li>昵称:<div class="p_present"><?php echo $user_info['nick_name']; ?></div></li>
+            </ul>
+        </div>
+        <div class="p_data">
+            <ul>
+                <li>所在地区:<div class="p_present">上海</div></li>
+                <li><i class="icon-camera-retro"></i>个人介绍:<div class="p_present">大脚好！</div></li>
+            </ul>
+        </div>
+        <div class="p_data">
+            <ul>
+                <li>兴趣标签:
+            <?php foreach ($user_info['interest'] as $single_interest): ?>
+            <div class="p_present" style="display:block"><?php echo $single_interest['name']; ?></div>
+        <?php endforeach; ?>
+        </li>
+        </ul>
+    </div>
+</div>
+</div>
+<!--个人中心主页-->
 
-</table>
+
+<div class="Clear"><!-- 清除浮动 --></div>
