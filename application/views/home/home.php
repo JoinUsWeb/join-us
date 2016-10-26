9bd44848eaaa0266fec3ddf4ae03169f88c01632
AndersonZhangyq <zhangyiqing818@sina.com>
2016-10-26 20:42:25 +0800

    Small promotion

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -11,39 +11,71 @@ $class_array = array(
 );
 ?>
 
+<script type="text/javascript">
+	var w,h,className;
+	function getSrceenWH(){
+		w = $(window).width();
+		h = $(window).height();
+		$('#dialogBg').width(w).height(h);
+	}
+
+	window.onresize = function(){
+		getSrceenWH();
+	}
+	$(window).resize();
+
+	$(function(){
+		getSrceenWH();
+
+
+		$('.box a').click(function(){
+			className = $(this).attr('class');
+			$('#dialogBg').fadeIn(300);
+			$('#dialog').removeAttr('class').addClass('animated '+className+'').fadeIn();
+		});
+
+
+		$('.claseDialogBtn').click(function(){
+			$('#dialogBg').fadeOut(300,function(){
+				$('#dialog').addClass('bounceOutUp').fadeOut();
+			});
+		});
+	});
+</script>
+
 <!-- Hot activities MUST be shown here!  -->
-<div class="container" xmlns="http://www.w3.org/1999/html">
+<div class="container">
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 hidden-xs">
             <div id="carousel-299058" class="carousel slide">
                 <ol class="carousel-indicators">
-                    <li data-target="#carousel-299058" data-slide-to="0" class=""></li>
-                    <li data-target="#carousel-299058" data-slide-to="1" class="active"></li>
-                    <li data-target="#carousel-299058" data-slide-to="2" class=""></li>
+                    <li data-target="#carousel-299058" data-slide-to="0" class=""> </li>
+                    <li data-target="#carousel-299058" data-slide-to="1" class="active"> </li>
+                    <li data-target="#carousel-299058" data-slide-to="2" class=""> </li>
                 </ol>
-                <!--<div class="carousel-inner">
+                <div class="carousel-inner">
                     <div class="item">
                         <img class="img-responsive"
                              src="<?php echo base_url($hot_activity[$hot_activity_count]['poster']); ?>"
                              alt="thumb">
                         <div class="carousel-caption"><?php echo $hot_activity[$hot_activity_count]['name'];
-                $hot_activity_count++; ?></div>
+                            $hot_activity_count++; ?></div>
                     </div>
                     <div class="item active">
                         <img class="img-responsive"
                              src="<?php echo base_url($hot_activity[$hot_activity_count]['poster']); ?>"
                              alt="thumb">
                         <div class="carousel-caption"><?php echo $hot_activity[$hot_activity_count]['name'];
-                $hot_activity_count++; ?></div>
+                            $hot_activity_count++; ?></div>
                     </div>
                     <div class="item">
                         <img class="img-responsive"
                              src="<?php echo base_url($hot_activity[$hot_activity_count]['poster']); ?>"
                              alt="thumb">
                         <div class="carousel-caption"><?php echo $hot_activity[$hot_activity_count]['name'];
-                $hot_activity_count++; ?></div>
+                            $hot_activity_count++; ?></div>
                     </div>
-                </div>-->
+                </div>
                 <?php $hot_activity_count = 0; ?>
                 <a class="left carousel-control" href="#carousel-299058" data-slide="prev"><span
                         class="icon-prev"></span></a> <a class="right carousel-control" href="#carousel-299058"
@@ -135,6 +167,8 @@ $class_array = array(
             if ($recommended_activity_count % 3 != 0) : ?>
         </div>
         <?php endif; ?>
+        <!-- 推荐的活动在此处结束 -->
+
         <hr>
         <div>
             <div>
@@ -153,204 +187,153 @@ $class_array = array(
                             <p><i class="icon-time icon-large"></i>2016-11-23 13:00</p>
                             <p><i class="icon-map-marker icon-large"></i>华东师范大学</p>
 
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
-                    <div class="thumbnail">
-                        <a href="html/details_page.html"><img src="img/400X200.gif" alt="Thumbnail Image 1"
-                                                              class="img-responsive"></a>
-                        <div class="caption">
-                            <a class="hd_title_block" href="html/details_page.html">
-                                <h3 class="hd_title">华东师范大学第二届创新创业大赛</h3>
-                            </a>
-                            <hr>
-                            <p><i class="icon-time icon-large"></i>2016-11-23 13:00</p>
-                            <p><i class="icon-map-marker icon-large"></i>华东师范大学</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
+						<div class="thumbnail">
+							<a href="html/details_page.html"><img src="img/400X200.gif" alt="Thumbnail Image 1" class="img-responsive"></a>
+							<div class="caption">
+								<a class="hd_title_block" href="html/details_page.html">
+									<h3 class="hd_title">华东师范大学第二届创新创业大赛</h3>
+								</a>
+								<hr>
+								<p><i class="icon-time icon-large"></i>2016-11-23 13:00</p>
+								<p><i class="icon-map-marker icon-large"></i>华东师范大学</p>
 
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-6 hidden-sm hidden-xs">
-                    <div class="thumbnail">
-                        <a href="html/details_page.html"><img src="img/400X200.gif" alt="Thumbnail Image 1"
-                                                              class="img-responsive"></a>
-                        <div class="caption">
-                            <a class="hd_title_block" href="html/details_page.html">
-                                <h3 class="hd_title">华东师范大学第二届创新创业大赛</h3>
-                            </a>
-                            <hr>
-                            <p><i class="icon-time icon-large"></i>2016-11-23 13:00</p>
-                            <p><i class="icon-map-marker icon-large"></i>华东师范大学</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6 hidden-sm hidden-xs">
+						<div class="thumbnail">
+							<a href="html/details_page.html"><img src="img/400X200.gif" alt="Thumbnail Image 1" class="img-responsive"></a>
+							<div class="caption">
+								<a class="hd_title_block" href="html/details_page.html">
+									<h3 class="hd_title">华东师范大学第二届创新创业大赛</h3>
+								</a>
+								<hr>
+								<p><i class="icon-time icon-large"></i>2016-11-23 13:00</p>
+								<p><i class="icon-map-marker icon-large"></i>华东师范大学</p>
 
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
-                    <div class="thumbnail">
-                        <a href="html/details_page.html"><img src="img/400X200.gif" alt="Thumbnail Image 1"
-                                                              class="img-responsive"></a>
-                        <div class="caption">
-                            <a class="hd_title_block" href="html/details_page.html">
-                                <h3 class="hd_title">华东师范大学第二届创新创业大赛</h3>
-                            </a>
-                            <hr>
-                            <p><i class="icon-time icon-large"></i>2016-11-23 13:00</p>
-                            <p><i class="icon-map-marker icon-large"></i>华东师范大学</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
+						<div class="thumbnail">
+							<a href="html/details_page.html"><img src="img/400X200.gif" alt="Thumbnail Image 1" class="img-responsive"></a>
+							<div class="caption">
+								<a class="hd_title_block" href="html/details_page.html">
+									<h3 class="hd_title">华东师范大学第二届创新创业大赛</h3>
+								</a>
+								<hr>
+								<p><i class="icon-time icon-large"></i>2016-11-23 13:00</p>
+								<p><i class="icon-map-marker icon-large"></i>华东师范大学</p>
 
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
-                    <div class="thumbnail">
-                        <a href="html/details_page.html"><img src="img/400X200.gif" alt="Thumbnail Image 1"
-                                                              class="img-responsive"></a>
-                        <div class="caption">
-                            <a class="hd_title_block" href="html/details_page.html">
-                                <h3 class="hd_title">华东师范大学第二届创新创业大赛</h3>
-                            </a>
-                            <hr>
-                            <p><i class="icon-time icon-large"></i>2016-11-23 13:00</p>
-                            <p><i class="icon-map-marker icon-large"></i>华东师范大学</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
+						<div class="thumbnail">
+							<a href="html/details_page.html"><img src="img/400X200.gif" alt="Thumbnail Image 1" class="img-responsive"></a>
+							<div class="caption">
+								<a class="hd_title_block" href="html/details_page.html">
+									<h3 class="hd_title">华东师范大学第二届创新创业大赛</h3>
+								</a>
+								<hr>
+								<p><i class="icon-time icon-large"></i>2016-11-23 13:00</p>
+								<p><i class="icon-map-marker icon-large"></i>华东师范大学</p>
 
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-4 hidden-sm hidden-xs">
-                    <div class="thumbnail">
-                        <a href="html/details_page.html"><img src="img/400X200.gif" alt="Thumbnail Image 1"
-                                                              class="img-responsive"></a>
-                        <div class="caption">
-                            <a class="hd_title_block" href="html/details_page.html">
-                                <h3 class="hd_title">华东师范大学第二届创新创业大赛</h3>
-                            </a>
-                            <hr>
-                            <p><i class="icon-time icon-large"></i>2016-11-23 13:00</p>
-                            <p><i class="icon-map-marker icon-large"></i>华东师范大学</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 hidden-sm hidden-xs">
+						<div class="thumbnail">
+							<a href="html/details_page.html"><img src="img/400X200.gif" alt="Thumbnail Image 1" class="img-responsive"></a>
+							<div class="caption">
+								<a class="hd_title_block" href="html/details_page.html">
+									<h3 class="hd_title">华东师范大学第二届创新创业大赛</h3>
+								</a>
+								<hr>
+								<p><i class="icon-time icon-large"></i>2016-11-23 13:00</p>
+								<p><i class="icon-map-marker icon-large"></i>华东师范大学</p>
 
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 
-    </div>
+		</div>
 
-    <div class="col-lg-3 col-md-6 col-md-offset-3 col-lg-offset-0">
-        <div class="well">
-            <h3 class="text-center">我的消息</h3>
-            <hr>
-            <div>
-                <ul>
-                    <li>
-                        <img src="" alt="photo">
-                        <p>交作品</p><br>
-                        <p>时间：2016-11-7</p><br>
-                    </li>
-                    <li>
-                        <img src="" alt="photo">
-                        <p>交作品</p><br>
-                        <p>时间：2016-11-7</p><br>
-                    </li>
-                    <li>
-                        <img src="" alt="photo">
-                        <p>交作品</p><br>
-                        <p>时间：2016-11-7</p><br>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <hr>
-        <div class="well">
-            <h3 class="text-center">热门活动</h3>
-            <hr>
-            <div>
-                <ul>
-                    <li>
-                        <img src="" alt="photo">
-                        <p>院运会</p>
-                    </li>
-                    <li>
-                        <a href="html/details_page.html">
-                            <img src="" alt="photo">
-                            <p>创新创业大赛</p>
-                        </a>
+		<div class="col-lg-3 col-md-6 col-md-offset-3 col-lg-offset-0">
+			<div class="messagebox">
+				<div class="my_message">
+					<p>我的消息</p>
+				</div>
+				<div class="my_message_hd">
 
-                    </li>
-                    <li>
-                        <img src="" alt="photo">
-                        <p>可怕的辅修</p>
-                    </li>
-                </ul>
-            </div>
-        </div>
+					<p><i class="icon-calendar"></i>活动提醒<!--2016-11-23创新创业大赛--></p>
+				</div>
+				<div class="my_message_pl">
+					<p><i class="icon-comments"></i>评论提醒</p>
+				</div>
+				<div class="my_message_yq">
+					<p><i class="icon-user"></i>新的邀请</p>
+				</div>
+				<div class="my_message_sh">
+					<p><i class="icon-ok-sign"></i>审核通过<!--您创建的2016-11-23创新创业大赛已审核通过--></p>
+				</div>
 
-    </div>
+			</div>
+			<br>
+			<div class="well">
+				<div class="hot_hd">
+					<p>热门活动</p>
+				</div>
+
+				<div class="hot_hd_content">
+
+					<div class="content_li">
+						<div class="li_left">
+							<a href="html/details_page.html" ><img src="img/01.png" alt="" width="70px" height="70px"></a>
+						</div>
+						<div class="li_right">
+							<a class="li_right_title" href="html/details_page.html"><h5>校运会</h5></a>
+							<p>2016-11-2 9:00</p>
+						</div>
+					</div>
 
 
-</div>
-</div>
+
+					<div class="content_li">
+						<div class="li_left">
+							<a href="html/details_page.html" ><img src="img/01.png" alt="" width="70px" height="70px"></a>
+						</div>
+						<div class="li_right">
+							<a class="li_right_title" href="html/details_page.html"><h5>华东师范大学第二届创新创业大赛</h5></a>
+							<p>2016-11-23 13:00</p>
+						</div>
+					</div>
 
 
-<hr>
-<div class="container well">
-    <div class="row">
-        <div class="col-xs-6 col-sm-6 col-lg-4 col-md-4"> <span class="text-right">
-		</span>
-            <h3>About Us</h3>
-            <hr>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, consequatur neque exercitationem
-                distinctio esse! Cupiditate doloribus a consequatur iusto illum eos facere vel iste iure maxime.
-                Velit,
-                rem, sunt obcaecati eveniet id nemo molestiae. In.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, consequatur neque exercitationem
-                distinctio esse! Cupiditate doloribus a consequatur iusto illum eos facere vel iste iure maxime.
-                Velit,
-                rem, sunt obcaecati eveniet id nemo molestiae. In.</p>
-        </div>
-        <div class="col-xs-6 col-sm-6 col-lg-4 col-md-4 hidden-sm hidden-xs"><span class="text-right"> </span>
-            <h3>Latest News</h3>
-            <hr>
-            <div class="media-object-default">
-                <div class="media">
-                    <div class="media-body">
-                        <h4 class="media-heading">Heading 1</h4>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, quod temporibus
-                        veniam
-                        deserunt deleniti accusamus voluptatibus at illo sunt quisquam.
-                    </div>
-                    <div class="media-right"><a href="#"> <img class="media-object" src="img/75X.gif"
-                                                               alt="placeholder image"></a></div>
-                </div>
-                <div class="media">
-                    <div class="media-body">
-                        <h4 class="media-heading">Heading 2</h4>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, iure nemo earum quae
-                        aliquid
-                        animi eligendi rerum rem porro facilis.
-                    </div>
-                    <div class="media-right"><a href="#"> <img class="media-object" src="img/75X.gif"
-                                                               alt="placeholder image"></a></div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xs-6 col-sm-6 col-lg-4 col-md-4"><span class="text-right"> </span>
-            <h3>Contact Us</h3>
-            <hr>
+					<div class="content_li">
+						<div class="li_left">
+							<a href="html/details_page.html" ><img src="img/01.png" alt="" width="70px" height="70px"></a>
+						</div>
+						<div class="li_right">
+							<a class="li_right_title" href="html/details_page.html"><h5>可怕的辅修</h5></a>
+							<p>2016-10-28 18:00</p>
+						</div>
+					</div>
 
-            <address>
-                <strong>MyStoreFront, Inc.</strong><br>
-                Indian Treasure Link<br>
-                Quitman, WA, 99110-0219<br>
-                <abbr title="Phone">P:</abbr> (123) 456-7890
-            </address>
+				</div>
+			</div>
 
-            <address>
-                <strong>Full Name</strong><br>
-                <a href="mailto:#">first.last@example.com</a>
-            </address>
-        </div>
-    </div>
+		</div>
+
+
+
+	</div>
 </div>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -9,5 +9,16 @@
     <link rel="stylesheet" href="<?php echo base_url("css/detail.css"); ?>">
     <link rel="stylesheet" href="<?php echo base_url("css/login&register.css"); ?>">
     <link rel="stylesheet" href="<?php echo base_url("css/personal.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("css/common.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("css/mainpage.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("css/login&register.css"); ?>">
+
+    <script type="text/javascript" src="<?php echo base_url("js/jquery.min.js"); ?>"></script>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
