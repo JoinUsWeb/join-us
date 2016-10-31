e72968e25b04c4cb0798d03238c839ecf3a77d35
Huge <1004050187@qq.com>
2016-10-31 23:09:06 +0800

    修正了注销的一个错误

--- application/controllers/Log_off.php
+++ application/controllers/Log_off.php
@@ -11,11 +11,12 @@ class Log_off extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->helper('url');
     }
 
     public function index()
     {
         unset($_SESSION['user_id']);
-        redirect(site_url('home'));
+        redirect(site_url('home/index'));
     }
 }
