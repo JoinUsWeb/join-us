67cbbfa50bdb5f503a57e235424468dd5c2d5391
lbe0613 <853601704@qq.com>
2017-02-15 19:14:28 +0800

    还没改好的验证

--- js/form_validation/login.js
+++ js/form_validation/login.js
@@ -17,13 +17,14 @@ function check_email() {
     var email_text = document.getElementById('email').value;
     if (email_text.length <= 0 || email_text.trim() == 0) {
         // 显示错误信息
-        alert("邮箱不能为空或全为空格！");
+        document.getElementById("email_error").innerHTML="邮箱不能为空或全为空格！";
+        $("#email_error").css("height",10px);
         return;
     }
     var reg = new RegExp("(([a-zA-Z]?[0-9]+)|([a-zA-Z]+[0-9]?))@([a-zA-z0-9]{1,}.){1,3}[a-zA-z]{1,}");
     if (!reg.test(email_text)) {
         // 显示错误提示
-        alert("邮箱格式错误！");
+        document.getElementById("email_error").innerHTML="邮箱格式错误！";
         return;
     }
     // 错误信息置为空
@@ -32,9 +33,9 @@ function check_email() {
 function check_password() {
     password_check = false;
     var password = document.getElementById('password').value;
-    if (password.length <= 0) {
+    if (password.length <= 6) {
         // 显示错误信息
-        alert("请输入密码！");
+        document.getElementById("psd_error").innerHTML="请输入密码！";
         return;
     }
     // 错误信息置为空
