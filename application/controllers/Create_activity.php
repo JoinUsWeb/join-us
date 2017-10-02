41ef18e2c725fb9a28f4f27422a17e9214dda7c4
huge0x0 <1004050187@qq.com>
2017-10-02 13:55:43 +0800

    调整上传图片限制

--- application/controllers/Create_activity.php
+++ application/controllers/Create_activity.php
@@ -18,9 +18,9 @@ class Create_activity extends CI_Controller
 
         $config['upload_path'] = FCPATH . '/img/';
         $config['allowed_types'] = 'gif|jpg|png';
-        $config['max_size'] = 2048;
-        $config['max_width'] = 1920;
-        $config['max_height'] = 1360;
+        $config['max_size'] = 4096;
+        $config['max_width'] = 3000;
+        $config['max_height'] = 3000;
         $this->load->library('upload', $config);
     }
 
